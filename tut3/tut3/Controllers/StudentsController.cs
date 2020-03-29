62d016e6780dd5e2c0e18a0cb42ccb0fec5409ae
unknown <s19200@pjwstk.edu.pl>
2020-03-29 12:21:28 +0200

    initial commit

---
+++ tut3/tut3/Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tut3.DAL;
+using tut3.Models;
+
+namespace tut3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Student not found");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult put(int id)
+        {
+            return Ok("Update complete");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult delete(int id)
+        {
+            return Ok("Delete completed");
+        }
+    }
+}
---
+++ tut3/tut3/DAL/IDbService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tut3.Models;
+
+namespace tut3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+
+    }
+}
